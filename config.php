c2c77736479baefe6b02c1416fd18a4c1f1ab296
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2022-01-01 23:38:47 +0600

    init

--- config.php
+++ config.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
@@ -36,42 +18,3 @@ define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-
-/**
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'xJyO3dV6vKN4g/nKmxl1YXFAjcQdxEPj7yAo5ZlqZMHNmDiikjg99TSRVJpgFG5PxUY1ayKgGPF5UceBy3jaeQ==');
-define('SECURE_AUTH_KEY',  'yfPy5B6gzxmmYsMmFHpsVP1J+GzLB4BzPSFbxwG8iaaIsCxe4NORjSCtkkX819nZ4LlUC9OaEO5RS/GgjW6ZzA==');
-define('LOGGED_IN_KEY',    '7A7rV6jdpPOSGKy5NENJXJBTiABDDZasxPFAmUvTKpM39fJYMSsNFM9E4cj85D5Cwy6zbIcRXTy0/yKeJ26iUg==');
-define('NONCE_KEY',        'EeKhGD4bDhEGOgZOXlLfiXjYQqYY5TXKLJtai2fQw+CKvAGKDLWYp1bHRKeE4egwEHkxKwUAe6gWZwpgC51jkQ==');
-define('AUTH_SALT',        'zvsWJo6/luYpnbJhzkpYkiQegQ2XSeSJbgvWI3CHSIKwT1O/1mgsHuMx/XRDdU08BAUEgxnFbS6EndxN0PHfYA==');
-define('SECURE_AUTH_SALT', 'byxrOm4smlD3qpaj1ZuCaHx5b+yPBu1R10sblmw36Ze8YTinuN28PBmbk8RjARUxqljx/9CQAjYJtv8j5hTHgQ==');
-define('LOGGED_IN_SALT',   'yidxnYnZXpnsbKUroyHpsHGUjHora2FgQYLtAYnd2hY1UXGJyhIZK9TLV7aGTMnq+9lkmvo/TexxbCNNWVlZwg==');
-define('NONCE_SALT',       'tt52jjAYce00WOvLTRL/DIbDBZdDudpwngwkTa51UykqJOzA7f0qk9qpB6VN53BF+qR8bN4DFOueSiOEoctNfw==');
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = 'wp_';
-
-
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
-}
-
-/** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
--- index.php
+++ index.php
@@ -1,17 +1,56 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Helping Wall</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+    
+<!-- Start Header -->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="javascript:void(0)">HW</a>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+        <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                </li>
+            </ul>
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- Start Footer -->
+
+<!-- Start Footer -->
+<footer class="py-3 my-4">
+    <div class="border-bottom"></div>
+    <div class="container">
+        <p class="text-center text-muted">©2022 All Rights Reserved</p>
+    </div>
+</footer>
+<!-- End Footer -->
+
+
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
