41383f1842f427157a948338948b556b5089ee30
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2022-05-30 02:18:40 +0600

    som

--- view-donation.php
+++ view-donation.php
@@ -94,6 +94,49 @@ $get_images = hw_get_images( $args['images'] );
 			</div>
 			<!-- End More Info -->
 
+      		<div class="card mb-4">
+      			<div class="card-body">
+				    <div class="d-flex row">
+				        <div class="col-md-12">
+				            <div class="d-flex flex-column comment-section">
+				                <div class="bg-white p-2">
+				                    <div class="d-flex flex-row user-info">
+				                    	<img class="rounded-circle" src="https://www.w3schools.com/bootstrap5/img_avatar3.png" width="40" height="40">
+				                        <div class="d-flex flex-column justify-content-start ms-2"><span class="d-block font-weight-bold name">Marry Andrews</span><span class="date text-black-50">Shared publicly - Jan 2020</span></div>
+				                    </div>
+				                    <div class="mt-2">
+				                        <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				                    </div>
+				                </div>
+				               
+				            </div>
+				        </div>
+				    </div>
+			    </div>
+			</div>
+			
+      		<div class="card mb-4">
+      			<div class="card-body">
+				    <div class="d-flex row">
+				        <div class="col-md-12">
+				            <div class="d-flex flex-column comment-section">
+				                <div class="bg-white p-2">
+				                    <div class="d-flex flex-row user-info">
+				                    	<img class="rounded-circle" src="https://www.w3schools.com/bootstrap5/img_avatar3.png" width="40" height="40">
+				                        <div class="d-flex flex-column justify-content-start ms-2"><span class="d-block font-weight-bold name">Marry Andrews</span><span class="date text-black-50">Shared publicly - Jan 2020</span></div>
+				                    </div>
+				                    <div class="mt-2">
+				                        <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				                    </div>
+				                </div>
+				               
+				            </div>
+				        </div>
+				    </div>
+			    </div>
+			</div>
+			<!-- End More Info -->
+
       	</div>
 
       	<div class="col-md-4 col-lg-4">
