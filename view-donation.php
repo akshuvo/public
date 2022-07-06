1424728dc9791740d058e2d307192d45c7ca0aa4
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2022-07-06 13:12:33 +0600

    fix slider

--- view-donation.php
+++ view-donation.php
@@ -41,32 +41,25 @@ $current_user = hw_parse_args($current_user, get_user_db_default_args());
 	<div class="row">
       	<div class="col-md-8 col-lg-8">
       		<div class="donation-images-section mb-5">
-				<div id="carouselExampleIndicators" class="carousel slide single-donation-carousel card" data-bs-ride="carousel">
-				    
-				    <div class="carousel-inner">
-				        <div class="carousel-item active">
-				            <img src="<?php echo home_url('/'); ?>/uploads/demo.png" class="d-block w-100" alt="...">
-				        </div>
-				        <div class="carousel-item">
-				            <img src="<?php echo home_url('/'); ?>/uploads/demo.png" class="d-block w-100" alt="...">
-				        </div>
-				        <div class="carousel-item">
-				            <img src="<?php echo home_url('/'); ?>/uploads/demo.png" class="d-block w-100" alt="...">
-				        </div>
-				    </div>
-				    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-				    
-				    </button>
-				    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-				   
-				    </button>
 
-				    <div class="carousel-indicators d-none">
-				        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-				        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-				    </div>
-				</div>
+      			<?php if( !empty( $args['images'] ) ) : ?>
+	                <div id="carouselExampleControls-<?php echo $args['id']; ?>" class="carousel slide single-donation-carousel card" data-bs-ride="carousel">
+	                    <div class="carousel-inner">
+	                        <?php foreach( $get_images as $key => $val ) : ?>
+	                            <div class="carousel-item <?php echo $key == "0" ? ' active ' : ' ';  ?>">
+	                            	<a href="<?php echo home_url('/') . $val['url']; ?>" target="_blank">
+	                                <img src="<?php echo home_url('/') . $val['url']; ?>" class="obfit-contain d-block w-100">
+	                                </a>
+	                            </div>
+	                        <?php endforeach; ?>
+	                    </div>
+	                    <button class="carousel-control-prev text-dark" type="button" data-bs-target="#carouselExampleControls-<?php echo $args['id']; ?>" data-bs-slide="prev"><i class="bi bi-chevron-left"></i></button>
+	                    <button class="carousel-control-next text-dark" type="button" data-bs-target="#carouselExampleControls-<?php echo $args['id']; ?>" data-bs-slide="next"><i class="bi bi-chevron-right"></i></button>
+	                </div>
+	            <?php else: ?>
+	                <img class="no-image-found" src="<?php echo home_url() . 'assets/img/no-image-found.png';?>" height="95">
+	            <?php endif; ?>
+
       		</div>	
       		<!-- End Carousel -->
 
